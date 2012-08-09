25f743cc6bd80d27ba0926b5fd7784d3d57651eb
chalupaul <paul@paulsresume.net>
2012-08-08 21:09:24 -0500

    added a check for screen

--- devstackhelper.sh
+++ devstackhelper.sh
@@ -4,6 +4,16 @@ then
     echo You must be root to run this.
     exit 1
 fi
+
+# Chicken and the egg problem with screen. Apt-get has a tendency to
+# eat your input buffer and thus is not very good at being
+# scriptified. Screen protects us from the tyranny of lousy
+# programming. This should be safe though (tested on ubuntu 12.04)
+
+if [ `dpkg -l screen 2>&1 | grep -vP "^No packages found" | wc -l` -eq 0 ]; then
+    apt-get -qy install screen
+fi
+
 GIT_PATH="${GIT_BASE:-https://github.com}/${GIT_NAME:-openstack}"
 screen apt-get update
 screen apt-get -qy install git emacs23-nox
