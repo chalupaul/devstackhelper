017e6f1d01e6842d169dad0584aa453adc22c41d
chalupaul <paul@paulsresume.net>
2012-08-08 21:48:25 -0500

    adding in DEBIAN_FRONTEND instead of screen to not eat the input.

--- devstackhelper.sh
+++ devstackhelper.sh
@@ -8,17 +8,9 @@ fi
 
 source genlocalrc.sh
 
-# Chicken and the egg problem with screen. Apt-get has a tendency to
-# eat your input buffer and thus is not very good at being
-# scriptified. Screen protects us from the tyranny of lousy
-# programming. This should be safe though (tested on ubuntu 12.04)
-
-if [ `dpkg -l screen 2>&1 | grep -vP "^No packages found" | wc -l` -eq 0 ]; then
-    apt-get -qy install screen
-fi
-
-screen apt-get update
-screen apt-get -qy install git emacs23-nox
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -qy install git emacs23-nox
 git clone https://github.com/openstack-dev/devstack
 cd devstack
 genlocalrc
